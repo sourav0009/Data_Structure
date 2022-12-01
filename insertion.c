24351859d22a9943d9c97442a0d273704e18ab9e
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2022-12-01 19:10:27 +0530

    Add files via upload

--- insertion.c
+++ insertion.c
@@ -1,40 +1,87 @@
-#include<stdio.h>
-void insertion_short(int ar[],int size,int temp)
+#include <stdio.h>
+#include <stdlib.h>
+void display(int size, int ar[])
 {
-    int i,j;
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        temp=ar[i];
-        j=i-1;
-        while(j>=0 && temp<ar[j])
-        {
-            ar[j+1]=ar[j];
-            j--;
-        }
-       ar[j+1]=temp;
-    }
-    for( int i=0;i<size;i++)
-    {
-        printf("%d\t",ar[i]);
+        printf("%d\t", ar[i]);
     }
 }
-
+void inscrtion_1st(int size, int ar[])
+{
+    int i, item;
+    printf("enter the no");
+    scanf("%d", &item);
+    for (i = size - 1; i >= 0; i--)
+    {
+        ar[i + 1] = ar[i];
+    }
+    ar[0] = item;
+    size++;
+    display(size, ar);
+}
+void insertion_pos(int size, int ar[])
+{
+    int i, item, pos;
+    printf("enter the no\t");
+    scanf("%d", &item);
+    printf("enter the pos\t");
+    scanf("%d", &pos);
+    for (i = size - 1; i >= pos - 1; i--)
+    {
+        ar[i + 1] = ar[i];
+    }
+    ar[pos - 1] = item;
+    size++;
+    display(size, ar);
+}
+void insertion_last(int size, int ar[])
+{
+    int i, item;
+    {
+    printf("enter the no\t");
+    scanf("%d", &item);
+    }
+    ar[size] = item;
+    size++;
+    display(size, ar);
+}
 void main()
 {
-    int i,j,size,ar[100],temp;
-    printf("enter the size ");
+    int c, size, ar[100];
+    printf("enter the size\t");
     scanf("%d",&size);
-    printf("enter the element");
-    for (int i = 0; i < size; i++)
+    printf("enter element\t");
+    for(int i=0;i<size;i++)
     {
         scanf("%d",&ar[i]);
     }
-    
-    printf("your array is");
+    printf("your enter element is\t");
     for(int i=0;i<size;i++)
     {
         printf("%d\t",ar[i]);
     }
-    printf("the inscration array is\n");
-   insertion_short(ar,size,temp);
+    while (1)
+    {
+        printf("enter 1 for insertion_1st\t enter 2 for insertion_pos\t enter 3 for insertion_last\t enter 4 for exist");
+        scanf("%d", &c);
+        switch (c)
+        {
+        case 1:
+            inscrtion_1st(size, ar);
+            break;
+        case 2:
+            insertion_pos(size, ar);
+            break;
+        case 3:
+            insertion_last(size, ar);
+            break;
+        case 4:
+            printf("program end");
+            exit(0);
+        default:
+            printf("wrong input");
+            break;
+        }
+    }
 }
